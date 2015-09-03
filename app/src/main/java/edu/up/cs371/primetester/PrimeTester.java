f525bbf6858432c1519dce61f2bc25742fc838d8
Phillip Manalili-Simeon <manalili18@up.edu>
2015-09-03 16:51:35 -0700

    Added funny picture and caption

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -1,4 +1,5 @@
 package edu.up.cs371.primetester;
+import android.util.Log;
 
 /**
  * Created by vegdahl on 7/17/2015.
@@ -13,18 +14,14 @@ package edu.up.cs371.primetester;
  * @param n the number to test
  * @return true if n is prime
  */
-public class PrimeTester
-{
-    public static boolean isPrime(long n)
-    {
+public class PrimeTester {
+    public static boolean isPrime(long n) {
+        Log.i("isPrime", "The input is "+n);
         if(n==1 || n==0) return false; //1 and 0 aren't prime
-
         long halfN = n/2;
-        for(int testMod=2;testMod<=halfN;++testMod)
-        {
+        for(int testMod=2;testMod<=halfN;++testMod) {
             if(n%testMod==0) return false;
         }
-
         return true;
-    }
+    }//isPrime
 }
