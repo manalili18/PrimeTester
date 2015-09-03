38e4559452aebba8db26e5d8a89d5cf11d631da5
Phillip Manalili-Simeon <manalili18@up.edu>
2015-09-03 16:02:16 -0700

    Fixed PrimeTester functionality

--- app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
+++ app/src/main/java/edu/up/cs371/primetester/PrimeTester.java
@@ -6,15 +6,25 @@ package edu.up.cs371.primetester;
  * The purpose of this class is to define a static method, isPrime, that determines whether an
  * integer is a prime number.
  */
-public class PrimeTester {
-    /**
-     * Tells whether an integer is prime.
-     *
-     * @param n the number to test
-     * @return true iff n is prime
-     */
-    public static boolean isPrime(long n) {
-        // for now, return a random result
-        return Math.random() > 0.5;
+
+/**
+ * Tells whether an integer is prime.
+ *
+ * @param n the number to test
+ * @return true if n is prime
+ */
+public class PrimeTester
+{
+    public static boolean isPrime(long n)
+    {
+        if(n==1 || n==0) return false; //1 and 0 aren't prime
+
+        long halfN = n/2;
+        for(int testMod=2;testMod<=halfN;++testMod)
+        {
+            if(n%testMod==0) return false;
+        }
+
+        return true;
     }
 }
